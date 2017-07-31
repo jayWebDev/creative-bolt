df8da591f0953908c0ea965d9761c3aeba7eccf3
Jason Shoreman <jaysdesign1@mac.com>
2017-07-30 22:34:08 -0400

    Updated page hero. Fixed SVG issue.

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,16 @@ var stylus = require('gulp-stylus');
 var pug = require('gulp-pug');
 var browserSync = require('browser-sync').create();
 var runSequence = require('run-sequence');
-var exec = require('child_process').exec;
+var cp = require('child_process');
 
-gulp.task('jekyll-build', function(cb) {
-  exec('jekyll build', function(err) {
-    if (err) return cb(err); // return error
-    cb();
-  });
+gulp.task('jekyll-build', function(code){
+  return cp.spawn('jekyll', ['build', '--incremental'], { stdio: 'inherit' })
+    .on('error', function(error) { gutil.log(gutil.colors.red(error.message))})
+    .on('close', code);
+})
+
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
 });
 
 gulp.task('browser-sync', function() {
@@ -40,10 +43,18 @@ gulp.task('styles', function() {
 });
 
 gulp.task('default', function(cb) {
-  runSequence(['views','styles'],'jekyll-build',cb);
+  runSequence(['views','styles'],'jekyll-build','watch',cb);
+});
+
+gulp.task('styles-seq', function(cb) {
+  runSequence('styles','jekyll-rebuild',cb);
+});
+
+gulp.task('views-seq', function(cb) {
+  runSequence('views','jekyll-rebuild',cb);
 });
 
 gulp.task('watch', ['browser-sync', 'styles', 'views'], function() {
-  gulp.watch('src/pug/*.pug', ['views']);
-  gulp.watch('src/stylus/*.styl', ['styles']);
+  gulp.watch('src/pug/*.pug', ['views-seq']);
+  gulp.watch('src/stylus/*.styl', ['styles-seq']);
 });
