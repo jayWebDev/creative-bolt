5cf9ed8957fef7e8cbb9cc2d9532eb57dafc7e9c
Jason Shoreman <jaysdesign1@mac.com>
2017-12-07 01:34:55 -0500

    Modified the main page to be a 1 page website. Added Flickity library for the project slide show.

---
+++ assets/scripts/script.js
@@ -0,0 +1,25 @@
+var flkty1 = new Flickity('#project-1', {
+  // options
+  cellAlign: 'center',
+  setGallerySize: false
+});
+
+var flkty2 = new Flickity('#project-2', {
+  // options
+  cellAlign: 'center',
+  setGallerySize: false,
+  rightToLeft: true
+});
+
+var flkty3 = new Flickity('#project-3', {
+  // options
+  cellAlign: 'center',
+  setGallerySize: false
+});
+
+var flkty4 = new Flickity('#project-4', {
+  // options
+  cellAlign: 'center',
+  setGallerySize: false,
+  rightToLeft: true
+});
